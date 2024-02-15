b7af02bd8083b0594b791bd2799edf0ef9047d33
Igor <igor.vguerra@hotmail.com>
2024-02-15 13:32:10 +0000

    query params

--- src/server.js
+++ src/server.js
@@ -6,10 +6,16 @@ app.get("/message/:id/:user", (request, response) => {
     
 const { id, user } = request.params;
     response.send(
-        `Mensagem ID: ${id}. 
+        `Mensagem ID: ${id}.
         Para o usuário: ${user}.
     `);
-})
+});
+
+app.get("/users", (request, response) => {
+    const { page, limit } = request.query;
+
+    response.send(`Página: ${page}. Mostrar: ${limit}`)
+});
 
 const PORT = 3333;
 
