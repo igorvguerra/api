b95436e5a5fa41a399eafec1fd1c7b1d07c3ed4d
Igor <igor.vguerra@hotmail.com>
2024-02-18 19:24:54 +0000

    learning about middleware

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,8 +4,20 @@ const UsersController = require("../controllers/UsersController");
 
 const usersRoutes = Router();
 
+function myMiddleware(request, response, next) {
+    console.log("Você passou pelo Middleware");
+    if(!request.body.isAdmin) {
+        return response.json({message: "user unauthorized"});
+    }
+    next();
+}  
+
+
+
+
+
 const userController = new UsersController();
 
-usersRoutes.post("/", userController.create);
+usersRoutes.post("/", myMiddleware, userController.create);
 
 module.exports = usersRoutes;
