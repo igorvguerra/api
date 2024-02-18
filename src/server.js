4d35268dace8f9482d96a77c08be8f7fe60b348d
Igor <igor.vguerra@hotmail.com>
2024-02-18 18:16:06 +0000

    using insomnia and learning post

--- src/server.js
+++ src/server.js
@@ -1,23 +1,15 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
-app.get("/message/:id/:user", (request, response) => {
-    
-const { id, user } = request.params;
-    response.send(
-        `Mensagem ID: ${id}.
-        Para o usuário: ${user}.
-    `);
-});
 
-app.get("/users", (request, response) => {
-    const { page, limit } = request.query;
+app.post("/users", (request, response) => {
+    const { name, email, password } = request.body;
 
-    response.send(`Página: ${page}. Mostrar: ${limit}`)
+    response.json({ name, email, password });
 });
 
 const PORT = 3333;
 
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
-
