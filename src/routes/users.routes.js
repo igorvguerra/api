44229a3b56e545410e262fdfc7e2fc7112e0dec4
Igor <igor.vguerra@hotmail.com>
2024-02-18 21:46:04 +0000

    working on errors

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,20 +4,8 @@ const UsersController = require("../controllers/UsersController");
 
 const usersRoutes = Router();
 
-function myMiddleware(request, response, next) {
-    console.log("Você passou pelo Middleware");
-    if(!request.body.isAdmin) {
-        return response.json({message: "user unauthorized"});
-    }
-    next();
-}  
-
-
-
-
-
 const userController = new UsersController();
 
-usersRoutes.post("/", myMiddleware, userController.create);
+usersRoutes.post("/", userController.create);
 
 module.exports = usersRoutes;
