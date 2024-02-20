1008cd2d6f7704e269d2a2c63c23828f57f17af9
Igor <igor.vguerra@hotmail.com>
2024-02-20 13:22:06 +0000

    update functions are complete

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require("bcryptjs");
+const { hash, compare } = require("bcryptjs");
 
 const AppError = require("../utils/AppError");
 
@@ -25,7 +25,7 @@ class UsersController {
     }
 
     async update(request, response){
-        const { name, email, password } = request.body;
+        const { name, email, password, old_password } = request.body;
         const { id } = request.params;
 
         const database = await sqliteConnection();
@@ -45,15 +45,29 @@ class UsersController {
 
         user.name = name;
         user.email = email;
+
+        if( password && !old_password){
+            throw new AppError("A senha atual é necessária para a definção de uma nova senha.");
+        }
        
+        if(password && old_password) {
+            const checkOldPassword = await compare(old_password, user.password);
+ 
+            if(!checkOldPassword){
+                throw new AppError("A senha atual está incorreta.");
+            }
+
+            user.password = await hash(password, 8);
+        }
 
         await database.run(`
             UPDATE users SET
             name = ?,
             email = ?,
-            updated_at = ?
+            password = ?,
+            updated_at = DATETIME('now')
             WHERE id = ?`, 
-            [user.name, user.email, new Date(), id]
+            [user.name, user.email, user.password, id]
         );
 
         return response.status(200).json();
