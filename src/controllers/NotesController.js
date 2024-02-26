5460b8243ec4c45c1bf8686462d428300b6b72f5
Igor <igor.vguerra@hotmail.com>
2024-02-26 17:21:05 +0000

    searching with tags

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -56,8 +56,20 @@ class NotesController {
     }
 
     async index(request, response) {
-        const { title, user_id } = request.query;
-        const notes = await knex("notes").where({ user_id }).whereLike("title", `%${title}%`).orderBy("title");
+        const { title, user_id, tags } = request.query;
+
+        let notes;
+
+        if(tags) {
+            const filterTags = tags.split(",").map(tag => tag.trim());
+            console.log(filterTags);
+
+            notes = await knex("tags").whereIn("name", filterTags);
+        
+        }else {
+            notes = await knex("notes").where({ user_id }).whereLike("title", `%${title}%`).orderBy("title");
+        }
+       
 
         return response.json(notes);
     }
