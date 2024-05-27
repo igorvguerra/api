43bccd055cb3be151945ee7c6c8aefd0b2e337d4
Igor <igor.vguerra@hotmail.com>
2024-05-27 21:53:42 +0100

    env.example

--- src/server.js
+++ src/server.js
@@ -36,6 +36,6 @@ app.use(( error, request, response, next) => {
 });
 
 
-const PORT = process.env.SERVER_PORT || 3000;
+const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
