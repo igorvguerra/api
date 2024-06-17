567285a2b426083de20b3c9edf3ba22731d114f7
Igor <igor.vguerra@hotmail.com>
2024-06-17 23:11:50 +0100

    before each test in jest

--- src/services/UserCreateService.spec.js
+++ src/services/UserCreateService.spec.js
@@ -1,20 +1,48 @@
 const UserCreateService = require("./UserCreateService");
 const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory");
+const AppError = require("../utils/AppError");
 
-it("user should be created", async () => {
-    const user = {
-        name: "User Test",
-        email: "user@test.com",
-        password: "123"
-    }
 
-    const userRepositoryInMemory = new UserRepositoryInMemory();
-    const userCreateService = new UserCreateService(userRepositoryInMemory);
+describe("UserCreateService", () => {
 
-    const userCreated = await userCreateService.execute(user);
+    let userRepositoryInMemory = null;
+    let userCreateService = null;
 
-    console.log(userCreated);
+    beforeEach(() => {
+        userRepositoryInMemory = new UserRepositoryInMemory();
+        userCreateService = new UserCreateService(userRepositoryInMemory);
+    });
 
-    expect(userCreated).toHaveProperty("id");
+    it("user should be created", async () => {
+        const user = {
+            name: "User Test",
+            email: "user@test.com",
+            password: "123"
+        }
+    
+        const userCreated = await userCreateService.execute(user);
+    
+        console.log(userCreated);
+    
+        expect(userCreated).toHaveProperty("id");
+    
+    });
 
+    it("user should not be create with an existing email", async () => {
+        const user1 = {
+            name: "User test 1",
+            email: "user@test.com",
+            password: "123"
+        };
+
+        const user2 = {
+            name: "User test 2",
+            email: "user@test.com",
+            password: "456"
+        };
+
+        await userCreateService.execute(user1);
+        await expect(userCreateService.execute(user2)).rejects.toEqual(new AppError("This e-mail is already in use."));
+    });
 });
+
